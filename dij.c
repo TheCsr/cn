2798f9687679b68bb456432f6991a5fa7a01b75c
Chandan Singh <csrrocz@gmail.com>
2017-12-12 13:34:58 -0500

    Update dij.c

--- dij.c
+++ dij.c
@@ -1,92 +1,46 @@
-#include<stdio.h>
-#include<conio.h>
-#define INFINITY 9999
-#define MAX 10
- 
-void dijkstra(int G[MAX][MAX],int n,int startnode);
- 
-int main()
+#include <stdio.h>
+#define infinity 999
+
+void dij(int n,int v,int cost[10][10],int dist[])
 {
-    int G[MAX][MAX],i,j,n,u;
-    printf("Enter no. of vertices:");
-    scanf("%d",&n);
-    printf("\nEnter the adjacency matrix:\n");
-    
-    for(i=0;i<n;i++)
-        for(j=0;j<n;j++)
-            scanf("%d",&G[i][j]);
-    
-    printf("\nEnter the starting node:");
-    scanf("%d",&u);
-    dijkstra(G,n,u);
-    
-    return 0;
+	int i,u,count,w,flag[10],min;
+ 	for(i=1;i<=n;i++)
+  		flag[i]=0,dist[i]=cost[v][i];
+ 	count=2;
+ 	while(count<=n)
+ 	{
+  		min=99;
+  		for(w=1;w<=n;w++)
+   			if(dist[w]<min && !flag[w])
+    			min=dist[w],u=w;
+  		flag[u]=1;
+  		count++;
+  		for(w=1;w<=n;w++)
+   			if((dist[u]+cost[u][w]<dist[w]) && !flag[w])
+    			dist[w]=dist[u]+cost[u][w];
+ 	}
 }
- 
-void dijkstra(int G[MAX][MAX],int n,int startnode)
+
+void main()
 {
+ 	int n,v,i,j,cost[10][10],dist[10];
+
+ 	printf("\n Enter the number of nodes:");
+ 	scanf("%d",&n);
+ 	printf("\n Enter the cost matrix:\n");
+ 	for(i=1;i<=n;i++)
+  		for(j=1;j<=n;j++)
+  		{
+   			scanf("%d",&cost[i][j]);
+   			if(cost[i][j]==0)
+    			cost[i][j]=infinity;
+  		}
+ 	printf("\n Enter the source node:");
+ 	scanf("%d",&v);
+ 	dij(n,v,cost,dist);
+ 	printf("\n Shortest path:\n");
+ 		for(i=1;i<=n;i++)
+  			if(i!=v)
+ 				printf("%d->%d,cost=%d\n",v,i,dist[i]);
  
-    int cost[MAX][MAX],distance[MAX],pred[MAX];
-    int visited[MAX],count,mindistance,nextnode,i,j;
-    
-    //pred[] stores the predecessor of each node
-    //count gives the number of nodes seen so far
-    //create the cost matrix
-    for(i=0;i<n;i++)
-        for(j=0;j<n;j++)
-            if(G[i][j]==0)
-                cost[i][j]=INFINITY;
-            else
-                cost[i][j]=G[i][j];
-    
-    //initialize pred[],distance[] and visited[]
-    for(i=0;i<n;i++)
-    {
-        distance[i]=cost[startnode][i];
-        pred[i]=startnode;
-        visited[i]=0;
-    }
-    
-    distance[startnode]=0;
-    visited[startnode]=1;
-    count=1;
-    
-    while(count<n-1)
-    {
-        mindistance=INFINITY;
-        
-        //nextnode gives the node at minimum distance
-        for(i=0;i<n;i++)
-            if(distance[i]<mindistance&&!visited[i])
-            {
-                mindistance=distance[i];
-                nextnode=i;
-            }
-            
-            //check if a better path exists through nextnode            
-            visited[nextnode]=1;
-            for(i=0;i<n;i++)
-                if(!visited[i])
-                    if(mindistance+cost[nextnode][i]<distance[i])
-                    {
-                        distance[i]=mindistance+cost[nextnode][i];
-                        pred[i]=nextnode;
-                    }
-        count++;
-    }
- 
-    //print the path and distance of each node
-    for(i=0;i<n;i++)
-        if(i!=startnode)
-        {
-            printf("\nDistance of node%d=%d",i,distance[i]);
-            printf("\nPath=%d",i);
-            
-            j=i;
-            do
-            {
-                j=pred[j];
-                printf("<-%d",j);
-            }while(j!=startnode);
-    }
 }
